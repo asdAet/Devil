88bd33b0bf2f282cb009c6e5877aef0e54995112
asdAet <101425999+asdAet@users.noreply.github.com>
2026-07-01 05:42:18 +0500

    Исправлен запуск daphne и сборки статики в контейнере

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
-exec daphne -b 0.0.0.0 -p 8000 --access-logfile - chat_app_django.asgi:application
+exec daphne -b 0.0.0.0 -p 8000 --access-log - chat_app_django.asgi:application
